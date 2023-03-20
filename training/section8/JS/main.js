aa4fc2fbca7a31b10486a1a6cd09fa0592818a96
“kabo2coffee” <fujiwaratomoya@fujiwaratomoyanoMacBook-Air.local>
2023-03-20 17:43:28 +0900

    axiosまで実装

--- training/section8/JS/main.js
+++ training/section8/JS/main.js
@@ -1,6 +1,37 @@
 const app = Vue.createApp({
   data: () => ({
-
+    items: null,
+    keyWord: "",
+    message: "",
   }),
-})
-app.mount('#app')
+  watch: {},
+  mounted: function () {
+    this.keyWord = "Vue";
+    this.getAnswer();
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.keyWord === "") {
+        console.log("キーワードが空です");
+        this.items = null;
+        return;
+      }
+      this.message = "Loading...";
+      const vm = this;
+      const params = { page: 1, perPage: 20, query: this.keyWord };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then(function (response) {
+          console.log(response);
+          vm.items = response.data;
+        })
+        .catch(function (error) {
+          vm.message = "Error!" + error;
+        })
+        .finally(function () {
+          vm.message = "";
+        });
+    },
+  },
+});
+app.mount("#app");
